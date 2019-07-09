3901914285a5cedb1be262f46db9b566e032aeb3
OlgaShitova <os@3d.ru>
2019-07-09 16:11:56 +0300

    инфо о системе апп

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+
+
+
+const {app, BrowserWindow} = require('electron'); // то, что мы извлекаем из электрона
+const path = require('path');
+const url = require('url');
+
+let win; //глобальная переменная reference to window object, зачем она нужна ниже
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname+'/img/logo.png',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+ win.loadURL(url.format(
+   {
+     pathname: path.join( __dirname + '/index.html'),
+     protocol:'file:',
+     slashes: true,
+     
+   }
+ ));
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  win.on('closed',  () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    win = null;
   })
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed',  () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') {
+    app.quit()
+  } // у windows компов это win32
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
